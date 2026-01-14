7f78baf521e5b650de15585c6bd9dabba366fd7d
KauanPires <kauanpiresgomes@gmail.com>
2026-01-14 13:35:41 -0300

    Project CREATED

---
+++ src/main/java/com/lk/AcaiPag/API/AcaiPagApiApplication.java
@@ -0,0 +1,13 @@
+package com.lk.AcaiPag.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcaiPagApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcaiPagApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lk/AcaiPag/API/AcaiPagApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lk.AcaiPag.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcaiPagApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
