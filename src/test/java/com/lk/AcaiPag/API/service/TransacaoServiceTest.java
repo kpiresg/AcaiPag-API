00819d530905cc89c0a5a2452f8c35d6d7e12976
KauanPires <kauanpiresgomes@gmail.com>
2026-01-18 17:19:44 -0300

    Test: Implementando teste de Saldo Insuficiente Exception

--- src/test/java/com/lk/AcaiPag/API/service/TransacaoServiceTest.java
+++ src/test/java/com/lk/AcaiPag/API/service/TransacaoServiceTest.java
@@ -3,10 +3,13 @@ package com.lk.AcaiPag.API.service;
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.lk.AcaiPag.API.exception.ContaNotFoundException;
+import com.lk.AcaiPag.API.exception.SaldoInsuficienteException;
+import com.lk.AcaiPag.API.model.Conta;
 import com.lk.AcaiPag.API.repository.ContaRepository;
 import com.lk.AcaiPag.API.repository.TransacaoRepository;
 import java.math.BigDecimal;
 import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -24,6 +27,7 @@ class TransacaoServiceTest {
   ContaRepository contaRepository;
 
   @Test
+  @DisplayName("Teste de Conta nao Encontrada")
   void testeDeContaNaoEncontradaException() {
     Mockito.when(contaRepository.findById(Mockito.anyLong())).thenReturn(Optional.empty());
     assertThrows(ContaNotFoundException.class,
@@ -31,6 +35,25 @@ class TransacaoServiceTest {
             1l,
             2l,
             BigDecimal.valueOf(10)));
+  }
+
+  @Test
+  @DisplayName("Teste de Saldo Insuficiente")
+  void testeDeSaldoInsuficienteException() {
+
+    Mockito.when(contaRepository.findById(Mockito.anyLong()))
+        .thenReturn(Optional.of(
+        new Conta(
+            1l,
+            "kauan",
+            BigDecimal.valueOf(1),
+            "123")));
+
+    assertThrows(SaldoInsuficienteException.class,
+        () ->  transacaoService.realizarTransacao(
+            1l,
+            2l,
+            BigDecimal.valueOf(2)));
   }
 
 }
