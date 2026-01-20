47f9c0ed313f9e8d0d52bd7d864d481c3a9d4f35
KauanPires <kauanpiresgomes@gmail.com>
2026-01-20 17:12:09 -0300

    test: verificar se gerou comprovante

--- src/test/java/com/lk/AcaiPag/API/service/TransacaoServiceTest.java
+++ src/test/java/com/lk/AcaiPag/API/service/TransacaoServiceTest.java
@@ -2,9 +2,11 @@ package com.lk.AcaiPag.API.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.lk.AcaiPag.API.dto.TransacaoDTO;
 import com.lk.AcaiPag.API.exception.ContaNotFoundException;
 import com.lk.AcaiPag.API.exception.SaldoInsuficienteException;
 import com.lk.AcaiPag.API.model.Conta;
+import com.lk.AcaiPag.API.model.Transacao;
 import com.lk.AcaiPag.API.repository.ContaRepository;
 import com.lk.AcaiPag.API.repository.TransacaoRepository;
 import java.math.BigDecimal;
@@ -43,7 +45,6 @@ class TransacaoServiceTest {
   @Test
   @DisplayName("Teste de Saldo Insuficiente")
   void testeDeSaldoInsuficienteException() {
-
     Mockito.when(contaRepository.findById(Mockito.anyLong()))
         .thenReturn(Optional.of(
         new Conta(
@@ -64,14 +65,27 @@ class TransacaoServiceTest {
   void testVerificarSeTransacaoOcorreuComSucesso() {
     Conta conta1 = new Conta(1l, "Kauan", BigDecimal.valueOf(100), "123");
     Conta conta2 = new Conta(2l, "Laura", BigDecimal.valueOf(200), "321");
-
     Mockito.when(contaRepository.findById(1l)).thenReturn(Optional.of(conta1));
     Mockito.when(contaRepository.findById(2l)).thenReturn(Optional.of(conta2));
 
     transacaoService.realizarTransacao(1l,2l,BigDecimal.valueOf(50));
 
+    assertNotNull(conta1.getValor());
     assertEquals(BigDecimal.valueOf(50), conta1.getValor());
     assertEquals(BigDecimal.valueOf(250), conta2.getValor());
+  }
+
+  @Test
+  @DisplayName("Teste verificar se gerou comprovante")
+  void testVerificarSeGerouComprovante() {
+    Conta conta1 = new Conta(1l, "Kauan", BigDecimal.valueOf(100), "123");
+    Conta conta2 = new Conta(2l, "Laura", BigDecimal.valueOf(200), "321");
+    Transacao transacao = new Transacao(conta1, conta2, BigDecimal.valueOf(100));
+
+    Mockito.when(transacaoRepository.save(Mockito.any(Transacao.class))).thenReturn(transacao);
+    Transacao resultado = transacaoRepository.save(transacao);
+
+    assertEquals(resultado, transacao);
   }
 
 }
