1ec684b9a23e34aabb697183d906ac1fa80f12f0
KauanPires <kauanpiresgomes@gmail.com>
2026-01-23 15:58:49 -0300

    Adicionando Postgree ao projeto

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO tb_conta (titular, valor, senha) VALUES ('Kauan Pires', 100.00005, '123')
-INSERT INTO tb_conta (titular, valor, senha) VALUES ('Laura Assis', 200.90, '654')
-INSERT INTO tb_conta (titular, valor, senha) VALUES ('Tamires', 5000, '987')
-INSERT INTO tb_conta (titular, valor, senha) VALUES ('Guilherme Gomes', 10000.50, '345')
+--INSERT INTO tb_conta (titular, valor, senha) VALUES ('Kauan Pires', 100.00005, '123')
+--INSERT INTO tb_conta (titular, valor, senha) VALUES ('Laura Assis', 200.90, '654')
+--INSERT INTO tb_conta (titular, valor, senha) VALUES ('Tamires', 5000, '987')
+--INSERT INTO tb_conta (titular, valor, senha) VALUES ('Guilherme Gomes', 10000.50, '345')
